e1f9def74679703d73a2288d7e91ea54947a2800
deviego <deviego4@gmail.com>
2023-09-14 17:10:23 -0300

    first commit

---
+++ src/app/components/ProductsCarts.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddToCart from './AddToCart'
+
+const ProductsCarts = () => {
+  return (
+    <div>
+      <h1>Products</h1>
+      <AddToCart/>
+    </div>
+  )
+}
+
+export default ProductsCarts
